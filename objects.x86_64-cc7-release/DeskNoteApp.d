a90096cfbaf7ae4b8253f6dd10311de70363db4e
trungnt2910 <trungnt282910@gmail.com>
2020-01-01 23:10:27 +0700

    Added Screenshot

--- objects.x86_64-cc7-release/DeskNoteApp.d
+++
@@ -1,5 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc7-release/DeskNoteApp.o : DeskNoteApp.h DeskNoteWindow.h \
-    DeskNoteView.h FontColourWindow.h DeskNoteTextView.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
